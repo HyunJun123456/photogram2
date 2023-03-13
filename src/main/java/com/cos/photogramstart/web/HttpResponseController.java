4ec569b7739eee0eecd3282a6fa9dbfea4b081e1
HyunJun123456 <juni303@chauniv.ac.kr>
2023-03-13 23:13:28 +0900

    Http 요청을 File로 응답하기

---
+++ src/main/java/com/cos/photogramstart/web/HttpResponseController.java
@@ -0,0 +1,23 @@
+package com.cos.photogramstart.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HttpResponseController {
+	@GetMapping("/txt")
+	public String txt() {
+		return "a.txt";
+	}
+	
+	@GetMapping("/mustache")
+	public String mustache() {
+		return "b";
+	}
+	
+	@GetMapping("/jsp")
+	public String jsp() {
+		return "c";
+	}
+	
+}
