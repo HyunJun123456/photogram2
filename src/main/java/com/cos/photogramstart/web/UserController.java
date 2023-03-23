b80c787f4af1b3b0f17420171d4980f47be817f8
HyunJun123456 <juni303@chauniv.ac.kr>
2023-03-23 21:18:06 +0900

    로그인 - 세션 정보 확인하기

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,13 +1,22 @@
 package com.cos.photogramstart.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+
 @Controller
 public class UserController {
 	@GetMapping("/user/{id}") // 프로필 페이지는 회원가입을 진행한 유저의 수만큼 존재하기 때문에 변수로써 존재해야함
-	public String profile(@PathVariable int id) { // 변수를 바인딩 할 수 있게 @PathVariable을 걸어줘야함
+	public String profile(@PathVariable int id,
+			@AuthenticationPrincipal PrincipalDetails principalDetails) { // 변수를 바인딩 할 수 있게 @PathVariable을 걸어줘야함
+		System.out.println("세션 정보 확인 : "+principalDetails.getUser());
+//		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+//		PrincipalDetails mPrincipalDetails = (PrincipalDetails) auth.getPrincipal();	
 		return "user/profile";
 	}
 	
